a45bac5adcfb7cca886560ddc461fcde76a7a055
twinklefingers <B1-66-ER@Rays-MBP.hsd1.mn.comcast.net>
2016-07-24 21:54:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+ $('button').on('click' function) {
+   $('main').show();
+   $('button').remove();
+  }); 
+});
+
+/*
+$(document).ready(function(){
+	$('.hidden').hide();
+	$('.clickMe').click(function () {
+	    if ($('.hidden').is(':hidden')) {
+	        $('.hidden').show();
+	    } else {
+	        $('.hidden').hide();
+	    }
+	}); 
+});
+
+$(document).ready(function(){
+	$('.hidden').hide();
+	$('.clickMe').on('click', function(){
+		$('.hidden').show();
+	});
+});
+
+Toggle
+
+mouseenter / mouseleave
+*/
+
+/*
+doc ready
+click something
+hide the button
+show the body
+*/
+
+/*
+Also want to check order of specificity
+make that image of myself scale
+add breaks, media query
+fix datetime
+*/
+
+/*
+A few things to watch out for:
+(2) time elements expect a 
+specific datetime format. 
+See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time
+*/
