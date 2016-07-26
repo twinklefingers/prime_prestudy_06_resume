be959026d4e4974e60ab7438bf900fe8dc220a54
twinklefingers <B1-66-ER@Rays-MacBook-Pro.local>
2016-07-26 11:26:50 -0500

    Linked jQuery

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
- $('button.clickMe').on('click', function) {
-   $('main.hidden').show();
-   $('button.clickMe').remove();
-  }); 
+	$('.hidden').hide();
+	$('.clickMe').click(function () {
+	    if ($('.hidden').is(':hidden')) {
+	        $('.hidden').show();
+	    } else {
+	        $('.hidden').hide();
+	    }
+	}); 
 });
 
 /*
 I tried all these... :/
+
+$(document).ready(function(){
+ $('button.clickMe').on('click', function() {
+   $('main.hidden').show();
+   $('button.clickMe').remove();
+  }); 
+});
 
 $(document).ready(function(){
  $('button').on('click', function) {
